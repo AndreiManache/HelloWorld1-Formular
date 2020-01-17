937e64c15e5848f0fe6d18ef749678779d72fec0
Manache Andrei <manacheandrei@gmail.com>
2020-01-17 13:04:08 +0200

    change1

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+function load(){
+    alert= document.querySelector(".alert");
+    console.log(alert);
+}
+
+
+function showMessage(){
+    
+   
+    alert.classList.remove('display');
+    
+}
