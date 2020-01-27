30695351d47452e5cc74c7e324ce46755b1d50ca
Manache Andrei <manacheandrei@gmail.com>
2020-01-27 15:23:44 +0200

    Final

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 
-var alert = document.querySelector(".alert");
-var names = document.querySelectorAll(".validate").value;
-var button = document.querySelector(".button");
 
-button.addEventListener('click',function showMessage(){
 
-    if(names === ""){
-       names.classList.add('redBorder'); 
+
+function validare(e){
+    e.preventDefault();
+
+    var nume1 = document.querySelector(".nameBox1").value;
+    var nume = document.querySelector(".nameBox").value;
+    var mesaj = document.querySelector(".message");
+    
+    if(nume1.length<3){
+            document.querySelector(".nameBox1").classList.add("invalid");
+          }else if(nume1.length>=3){
+            document.querySelector(".nameBox1").classList.remove("invalid");
+          }
+
+    if(nume.length<3){
+            document.querySelector(".nameBox").classList.add("invalid");
+          }else if(nume.length>=3){
+            document.querySelector(".nameBox").classList.remove("invalid");
+          }
+
+          if(nume.length >= 3 && nume1.length >= 3){
+
+            mesaj.innerText = 'Thank you for contacting us,'+" "+nume1+" "+ nume+"!";
+            mesaj.classList.remove("hideMessage");
+        
     }
-   
+
+}
+
     
-    
-})
